79ea463294b2126d4a7f6e46d26efb980346f959
DrazenRocket <drazen.rocket@hotmail.com>
2017-03-28 01:42:40 +0200

    Added method for process service.

--- PhDProcessServiceApp/app/components/process/process.service.js
+++ PhDProcessServiceApp/app/components/process/process.service.js
@@ -9,4 +9,74 @@
 		this.$http = $http;
 		this.$base64 = $base64;
 	}
+
+	ProcessService.property.getProcessDefinitionById = function (id, credentials, successcb, errorcb) {
+		var request = {
+			method: "GET",
+			url: "http://localhost:8080/activiti-rest/service/repository/process-definitions/" + id,
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json",
+				"Authorization": "Basic " + this.$base64.encode(credentials)
+			}
+		};
+
+		this.$http(request).then(successcb, errorcb);
+	};
+
+	ProcessService.prototype.getProcessDefinitionListAll = function (credentials, successcb, errorcb) {
+		var request = {
+			method: "GET",
+			url: "http://localhost:8080/activiti-rest/service/repository/process-definitions?size=1000",
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json",
+				"Authorization": "Basic " + this.$base64.encode(credentials)
+			}
+		};
+
+		this.$http(request).then(successcb, errorcb);
+	};
+
+	ProcessService.property.getProcessDefinitionListByName = function (name, credentials, successcb, errorcb) {
+		var request = {
+			method: "GET",
+			url: "http://localhost:8080/activiti-rest/service/repository/process-definitions?size=1000&name=" + name,
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json",
+				"Authorization": "Basic " + this.$base64.encode(credentials)
+			}
+		};
+
+		this.$http(request).then(successcb, errorcb);
+	};
+
+	ProcessService.property.getProcessDefinitionListByCategory = function (category, credentials, successcb, errorcb) {
+		var request = {
+			method: "GET",
+			url: "http://localhost:8080/activiti-rest/service/repository/process-definitions?size=1000&category=" + category,
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json",
+				"Authorization": "Basic " + this.$base64.encode(credentials)
+			}
+		};
+
+		this.$http(request).then(successcb, errorcb);
+	};
+
+	ProcessService.property.getProcessDefinitionListByStartableByUser = function(startableByUser, credentials, successcb, errorcb) {
+		var request = {
+			method: "GET",
+			url: "http://localhost:8080/activiti-rest/service/repository/process-definitions?size=1000&startableByUser=" + startableByUser,
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json",
+				"Authorization": "Basic " + this.$base64.encode(credentials)
+			}
+		};
+
+		this.$http(request).then(successcb, errorcb);
+	};
 } (angular));
