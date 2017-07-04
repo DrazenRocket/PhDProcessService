3f1d7a2ea6650cf2c8208b86dcf54842fef6425c
DrazenRocket <drazen.rocket@hotmail.com>
2017-07-04 23:44:12 +0200

    Fixed a bug: default state/route.

--- PhDProcessServiceApp/app/app.route.js
+++ PhDProcessServiceApp/app/app.route.js
@@ -1,6 +1,10 @@
 (function (angular) {
 	"use strict";
 
+	angular
+		.module("app")
+		.config(configRoute);
+
 	configRoute.$inject = ["$locationProvider", "$stateProvider", "$urlRouterProvider"];
 	function configRoute($locationProvider, $stateProvider, $urlRouterProvider) {
 		$locationProvider.hashPrefix("");
